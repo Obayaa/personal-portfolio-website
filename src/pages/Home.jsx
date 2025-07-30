eb3cd60d53e685cf6f77d4c57790aa68ae470646
Obayaa <doraappiah2004@gmail.com>
2025-07-30 08:55:42 +0000

    defined and added content to the various page sections of the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-function Home(){
+function Home() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-gray-800">Welcome to My Portfolio</h1>
-        <p className="mt-4 text-lg text-gray-600">This is the <span className="text-red-600">Home</span> page of my personal portfolio.</p>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
+            <h1 className="text-4xl font-bold text-gray-800">Welcome to My Portfolio</h1>
+            <p className="mt-4 text-lg text-gray-600">This is the <span className="text-red-600">Home</span> page of my personal portfolio.</p>
         </div>
     );
 }
