483c1cdd1341dcfe572476cb99552407e87ff364
Obayaa <doraappiah2004@gmail.com>
2025-07-30 08:55:01 +0000

    Added page sections to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow bg-gray-100">
+        <section className="" id="home"><Home /></section>
+        <section className="" id="about"><About /></section>
+        <section className="" id="skills"><Skills /></section>
+        <section className="" id="projects"><Projects /></section>
+        <section className="" id="contact"><Contact /></section>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
