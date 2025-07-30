08aa64530b5524ed40c3c9b434fef41828a25062
Obayaa <doraappiah2004@gmail.com>
2025-07-30 10:29:49 +0000

    updated the nav bar to use a transparent navbar and a bottom border

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
     return (
-        <nav className="bg-gray-800 p-4 fixed w-full top-0">
+        <nav className="bg-transparent border-b border-white/10 p-4 fixed w-full top-0">
             <div className="container mx-auto flex justify-between items-center">
                 <a href="/" className="text-white text-lg font-bold">My Portfolio</a>
                 <ul className="flex space-x-4">
